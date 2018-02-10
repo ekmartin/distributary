b1dcbb8d993f5961f10f26d0d92fedb8a9ac861a
Martin Ek <mail@ekmartin.com>
2018-02-10 18:46:21 +0100

    Define State as an enum
    Tried using a trait first, but wasn't sure how to fit it into the
    existing StateMap (which would then potentially need to hold multiple
    State types).

--- core/src/local/state.rs
+++ core/src/local/state.rs
@@ -5,26 +5,134 @@ use std::rc::Rc;
 use local::keyed_state::KeyedState;
 use local::single_state::SingleState;
 
+pub enum State {
+    InMemory(MemoryState),
+}
+
+impl State {
+    pub fn default() -> Self {
+        State::InMemory(MemoryState::default())
+    }
+
+    pub fn base() -> Self {
+        State::InMemory(MemoryState::default())
+    }
+
+    pub fn add_key(&mut self, columns: &[usize], partial: Option<Vec<Tag>>) {
+        match *self {
+            State::InMemory(ref mut memory_state) => memory_state.add_key(columns, partial),
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn keys(&self) -> Vec<Vec<usize>> {
+        match *self {
+            State::InMemory(ref memory_state) => memory_state.keys(),
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn is_useful(&self) -> bool {
+        match *self {
+            State::InMemory(ref memory_state) => memory_state.is_useful(),
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn is_partial(&self) -> bool {
+        match *self {
+            State::InMemory(ref memory_state) => memory_state.is_partial(),
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn insert(&mut self, r: Vec<DataType>, partial_tag: Option<Tag>) -> bool {
+        match *self {
+            State::InMemory(ref mut memory_state) => memory_state.insert(r, partial_tag),
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn remove(&mut self, r: &[DataType]) -> bool {
+        match *self {
+            State::InMemory(ref mut memory_state) => memory_state.remove(r),
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn iter(&self) -> rahashmap::Values<DataType, Vec<Row>> {
+        match *self {
+            State::InMemory(ref memory_state) => memory_state.iter(),
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        match *self {
+            State::InMemory(ref memory_state) => memory_state.is_empty(),
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        match *self {
+            State::InMemory(ref memory_state) => memory_state.len(),
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn nkeys(&self) -> usize {
+        match *self {
+            State::InMemory(ref memory_state) => memory_state.nkeys(),
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn mark_hole(&mut self, key: &[DataType], tag: &Tag) {
+        match *self {
+            State::InMemory(ref mut memory_state) => memory_state.mark_hole(key, tag),
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn mark_filled(&mut self, key: Vec<DataType>, tag: &Tag) {
+        match *self {
+            State::InMemory(ref mut memory_state) => memory_state.mark_filled(key, tag),
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn lookup<'a>(&'a self, columns: &[usize], key: &KeyType) -> LookupResult<'a> {
+        match *self {
+            State::InMemory(ref memory_state) => memory_state.lookup(columns, key),
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn cloned_records(&self) -> Vec<Vec<DataType>> {
+        match *self {
+            State::InMemory(ref memory_state) => memory_state.cloned_records(),
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn clear(&mut self) {
+        match *self {
+            State::InMemory(ref mut memory_state) => memory_state.clear(),
+            _ => unreachable!(),
+        }
+    }
+}
+
 #[derive(Default)]
-pub struct State {
+pub struct MemoryState {
     state: Vec<SingleState>,
     by_tag: HashMap<Tag, usize>,
     rows: usize,
 }
 
-impl State {
-    /// Construct base materializations differently (potentially)
-    pub fn base() -> Self {
-        Self::default()
-    }
-
-    /// Returns the index in `self.state` of the index keyed on `cols`, or None if no such index
-    /// exists.
-    fn state_for(&self, cols: &[usize]) -> Option<usize> {
-        self.state.iter().position(|s| &s.key[..] == cols)
-    }
-
-    pub fn add_key(&mut self, columns: &[usize], partial: Option<Vec<Tag>>) {
+impl MemoryState {
+    fn add_key(&mut self, columns: &[usize], partial: Option<Vec<Tag>>) {
         let (i, exists) = if let Some(i) = self.state_for(columns) {
             // already keyed by this key; just adding tags
             (i, true)
@@ -81,21 +189,21 @@ impl State {
     }
 
     /// Returns a Vec of all keys that currently exist on this materialization.
-    pub fn keys(&self) -> Vec<Vec<usize>> {
+    fn keys(&self) -> Vec<Vec<usize>> {
         self.state.iter().map(|s| &s.key).cloned().collect()
     }
 
     /// Returns whether this state is currently keyed on anything. If not, then it cannot store any
     /// infromation and is thus "not useful".
-    pub fn is_useful(&self) -> bool {
+    fn is_useful(&self) -> bool {
         !self.state.is_empty()
     }
 
-    pub fn is_partial(&self) -> bool {
+    fn is_partial(&self) -> bool {
         self.state.iter().any(|s| s.partial)
     }
 
-    pub fn insert(&mut self, r: Vec<DataType>, partial_tag: Option<Tag>) -> bool {
+    fn insert(&mut self, r: Vec<DataType>, partial_tag: Option<Tag>) -> bool {
         let r = Rc::new(r);
 
         if let Some(tag) = partial_tag {
@@ -120,7 +228,7 @@ impl State {
         }
     }
 
-    pub fn remove(&mut self, r: &[DataType]) -> bool {
+    fn remove(&mut self, r: &[DataType]) -> bool {
         let mut hit = false;
         let mut removed = false;
         let fix = |removed: &mut bool, rs: &mut Vec<Row>| {
@@ -207,7 +315,7 @@ impl State {
         hit
     }
 
-    pub fn iter(&self) -> rahashmap::Values<DataType, Vec<Row>> {
+    fn iter(&self) -> rahashmap::Values<DataType, Vec<Row>> {
         for index in &self.state {
             if let KeyedState::Single(ref map) = index.state {
                 if index.partial {
@@ -220,15 +328,15 @@ impl State {
         unimplemented!();
     }
 
-    pub fn is_empty(&self) -> bool {
+    fn is_empty(&self) -> bool {
         self.state.is_empty() || self.state[0].state.is_empty()
     }
 
-    pub fn len(&self) -> usize {
+    fn len(&self) -> usize {
         self.rows
     }
 
-    pub fn nkeys(&self) -> usize {
+    fn nkeys(&self) -> usize {
         if self.state.is_empty() {
             0
         } else {
@@ -236,7 +344,7 @@ impl State {
         }
     }
 
-    pub fn mark_filled(&mut self, key: Vec<DataType>, tag: &Tag) {
+    fn mark_filled(&mut self, key: Vec<DataType>, tag: &Tag) {
         debug_assert!(!self.state.is_empty(), "filling uninitialized index");
         let i = self.by_tag[tag];
         let index = &mut self.state[i];
@@ -288,7 +396,7 @@ impl State {
         assert!(replaced.is_none());
     }
 
-    pub fn mark_hole(&mut self, key: &[DataType], tag: &Tag) {
+    fn mark_hole(&mut self, key: &[DataType], tag: &Tag) {
         debug_assert!(!self.state.is_empty(), "filling uninitialized index");
         let i = self.by_tag[tag];
         let index = &mut self.state[i];
@@ -324,7 +432,7 @@ impl State {
         assert!(removed.is_some());
     }
 
-    pub fn lookup<'a>(&'a self, columns: &[usize], key: &KeyType) -> LookupResult<'a> {
+    fn lookup<'a>(&'a self, columns: &[usize], key: &KeyType) -> LookupResult<'a> {
         debug_assert!(!self.state.is_empty(), "lookup on uninitialized index");
         let index = &self.state[self.state_for(columns)
                                     .expect("lookup on non-indexed column set")];
@@ -356,7 +464,7 @@ impl State {
         }
     }
 
-    pub fn clear(&mut self) {
+    fn clear(&mut self) {
         self.rows = 0;
         for s in &mut self.state {
             match s.state {
@@ -370,6 +478,12 @@ impl State {
         }
     }
 
+    /// Returns the index in `self.state` of the index keyed on `cols`, or None if no such index
+    /// exists.
+    fn state_for(&self, cols: &[usize]) -> Option<usize> {
+        self.state.iter().position(|s| &s.key[..] == cols)
+    }
+
     fn unalias_for_state(&mut self) {
         let left = self.state.drain(..).last();
         if let Some(left) = left {
@@ -378,7 +492,7 @@ impl State {
     }
 }
 
-impl<'a> Drop for State {
+impl<'a> Drop for MemoryState {
     fn drop(&mut self) {
         self.unalias_for_state();
         self.clear();
