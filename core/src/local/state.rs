80c35ee505ddbe2933648d6d1bcd53ba9a538b79
Martin Ek <mail@ekmartin.com>
2018-03-06 21:01:55 -0800

    Open the sqlite db in exclusive mode

--- core/src/local/state.rs
+++ core/src/local/state.rs
@@ -159,6 +159,7 @@ impl PersistentState {
         connection
             .execute_batch(
                 "CREATE TABLE IF NOT EXISTS store (row BLOB);
+                PRAGMA locking_mode = EXCLUSIVE;
                 PRAGMA synchronous = NORMAL;
                 PRAGMA journal_mode = WAL;",
             )
