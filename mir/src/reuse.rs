fd07b7b71c0c4fa5d112601bb06f368f26fa08c5
Martin Ek <mail@ekmartin.com>
2018-03-25 23:12:31 +0200

    Try using &mut Logger for slog

--- mir/src/reuse.rs
+++ mir/src/reuse.rs
@@ -33,7 +33,7 @@ pub fn rewind_until_columns_found(leaf: MirNodeRef, columns: &Vec<Column>) -> Op
 }
 
 pub fn merge_mir_for_queries(
-    log: &slog::Logger,
+    log: &mut slog::Logger,
     new_query: &MirQuery,
     old_query: &MirQuery,
 ) -> (MirQuery, usize) {
@@ -287,7 +287,7 @@ mod tests {
         use node::{MirNode, MirNodeType};
         use query::MirQuery;
 
-        let log = slog::Logger::root(slog::Discard, o!());
+        let mut log = slog::Logger::root(slog::Discard, o!());
 
         let (a, b, c, d) = make_nodes();
 
@@ -306,7 +306,7 @@ mod tests {
         };
 
         // when merging with ourselves, the result should consist entirely of reuse nodes
-        let (merged_reflexive, _) = merge_mir_for_queries(&log, &mq1, &mq1);
+        let (merged_reflexive, _) = merge_mir_for_queries(&mut log, &mq1, &mq1);
         assert!(
             merged_reflexive
                 .topo_nodes()
@@ -342,7 +342,7 @@ mod tests {
             roots: vec![a, b],
             leaf: d,
         };
-        let (merged_extension, _) = merge_mir_for_queries(&log, &mq2, &mq1);
+        let (merged_extension, _) = merge_mir_for_queries(&mut log, &mq2, &mq1);
         for n in merged_extension.topo_nodes() {
             match n.borrow().name() {
                 // first three nodes (2x base, 1x join) should have been reused
